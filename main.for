20c34ab15ac711ae7c6a9d523f772b1d6616fe36
StevSpes <steffenspecker@web.de>
2023-02-08 00:09:23 +0100

    Fertigstellung Anhang und Inhaltsverzeichnis

---
+++ main.for
@@ -0,0 +1,10 @@
+\addvspace {10\p@ }
+\addvspace {10\p@ }
+\contentsline {formel}{\numberline {1}{\ignorespaces Grundformel 1\relax }}{5}{}%
+\contentsline {formel}{\numberline {2}{\ignorespaces Schwerkraftsberechnung\relax }}{5}{}%
+\contentsline {formel}{\numberline {3}{\ignorespaces Komplizierte Formel\relax }}{5}{}%
+\addvspace {10\p@ }
+\addvspace {10\p@ }
+\addvspace {10\p@ }
+\addvspace {10\p@ }
+\addvspace {10\p@ }
